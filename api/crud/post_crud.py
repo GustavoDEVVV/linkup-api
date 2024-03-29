c9c7cf487bcda5e94250eb8b567e116f0a5bf1d5
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-03-29 19:58:07 -0300

    feat: added select posts

--- api/crud/post_crud.py
+++ api/crud/post_crud.py
@@ -1,6 +1,17 @@
 from sqlalchemy.orm import Session
 from api.models import PostModel
+from api.validators.post_validators import PostCreate
 
 
-def select_posts(db: Session, user_id: int):
-    return db.query(PostModel).filter(PostModel.owner_id == user_id).all()
+def select_posts(db: Session, username: str):
+    return db.query(PostModel).filter(PostModel.owner_username == username).all()
+
+
+def insert_post(db: Session, post: PostCreate, username: str):
+    db_post = PostModel(**post.model_dump(), owner_username=username)
+
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+
+    return {"message": f"Post created at user = {db_post.id}"}
