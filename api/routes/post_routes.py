2ae5aaa1dd81ec19a30ac223c1e109eeccd9247e
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-03-29 13:39:12 -0300

    feat(routes): updated get route

--- api/routes/post_routes.py
+++ api/routes/post_routes.py
@@ -1,14 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
 from core.db import Base, engine
+from api.crud.post_crud import select_posts
+from api.deps import get_db
 
 Base.metadata.create_all(bind=engine)
 
 router = APIRouter(
-    prefix='/posts',
+    prefix='/users',
     tags=['posts']
 )
 
 
-@router.get('/')
-def read_users():
-    return 'rota de posts'
+@router.get('/{user_id}/posts/')
+async def get_posts(user_id: int, db: Session = Depends(get_db)):
+    posts = select_posts(db, user_id=user_id)
+    return posts
