35d70c49f72bd16d9ec9120834d8f5b7457ade93
Gustavo Domingues <142946339+GustavoDEVVV@users.noreply.github.com>
2024-03-30 11:43:26 -0300

    Atualização nos testes
    Post
    get, update
    
    users
    user_by_username, updata

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,3 +8,35 @@ class TestUserRoutes:
         response = requests.get(self.base_url)
         assert response.status_code == 200
         assert isinstance(response.json(), list)
+        
+    def test_get_user_by_username(self):
+        username = 'Gustavo'
+        response = request.get(f"{self.base_url}/{username}")
+        assert request.status_code == 200
+        assert isinstance(response.json, dict)
+
+    def test_update_user(self):
+        username = 'Gustavo'
+        update_data_users = {'name': 'Gustavo', 'email': 'gustavo@example.com', 'password': '1234'}
+        assert response.status_code == 200
+        updated_user = response.json()
+        assert updated_user['name'] == update_data_users['name']
+
+
+class TestPostRoutes:
+
+    base_url = 'http://127.0.0.1:8000/users/'
+
+    def get_posts(username):
+
+        url = f"{base_url}/users/{username}/posts/"
+        response = requests.get(url)
+        assert response.status_code == 200
+        return response
+   
+    def test_update_post():
+        id_post = 1
+        update_data_post = {'username': 'Gustavo', 'id_post': 2}
+        response = client.put(f"/posts/{id_post}", json=update_data_post)
+        assert response.status_code == 200
+        assert response.json()['id_post'] == update_data_post['id_post']
