79c264117bd5b81c11cbbb587aa34b3eb5075179
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-03-30 14:06:59 -0300

    :goal_net: adding error handling in routes

--- api/routes/user_routes.py
+++ api/routes/user_routes.py
@@ -23,47 +23,55 @@ router = APIRouter(
 
 @router.get('/', response_model=list)
 async def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    user_posts = []
-    users = select_users(db, skip=skip, limit=limit)
+    try:
+        user_posts = []
+        users = select_users(db, skip=skip, limit=limit)
+        for user in users:
+            user_dict = user.__dict__
+            user_dict['posts'] = [post.__dict__ for post in user.posts]
 
-    for user in users:
-        user_dict = user.__dict__
-        user_dict['posts'] = [post.__dict__ for post in user.posts]
+            user_dict.pop('id')
+            user_dict.pop('email')
+            user_dict.pop('password')
 
-        user_dict.pop('id')
-        user_dict.pop('email')
-        user_dict.pop('password')
+            user_dict.pop('is_admin')
+            user_dict.pop('is_active')
 
-        user_dict.pop('is_admin')
-        user_dict.pop('is_active')
+            user_posts.append(user_dict)
 
-        user_posts.append(user_dict)
+            for post in user_dict['posts']:
+                post.pop('id')
+                post.pop('owner_username')
 
-        for post in user_dict['posts']:
-            post.pop('id')
-            post.pop('owner_username')
-
-    return user_posts
+        return user_posts
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
 
 
 @router.post('/')
 async def post_user(user: UserCreate, db: Session = Depends(get_db)):
-    exceptions = []
+    try:
+        exceptions = []
+        user_exists_email = select_user_by_email(
+            db, email=user.email
+        )
+        user_exists_username = select_user_by_username(
+            db, username=user.username
+        )
+        if user_exists_email:
+            exceptions.append("Email already registered")
 
-    user_exists_email = select_user_by_email(db, email=user.email)
-    user_exists_username = select_user_by_username(db, username=user.username)
+        if user_exists_username:
+            exceptions.append("Username already registered")
 
-    if user_exists_email:
-        exceptions.append("Email already registered")
+        if len(exceptions) > 1:
+            raise HTTPException(status_code=400, detail=exceptions)
 
-    if user_exists_username:
-        exceptions.append("Username already registered")
+        elif len(exceptions) == 1:
+            raise HTTPException(status_code=400, detail=exceptions[0])
 
-    if len(exceptions) > 1:
-        raise HTTPException(status_code=400, detail=exceptions)
+        else:
+            return create_user(db=db, user=user)
 
-    elif len(exceptions) == 1:
-        raise HTTPException(status_code=400, detail=exceptions[0])
-
-    else:
-        return create_user(db=db, user=user)
+    except Exception as e:
+        return HTTPException(status_code=500, detail=str(e))
