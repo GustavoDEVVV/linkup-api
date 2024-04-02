2d2761a94326e8a4e6179eb4008204a798634189
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-04-02 16:08:05 -0300

    test: post routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,11 +10,11 @@ class TestUserRoutes:
         assert isinstance(response.json(), list)
 
 
-# class TestPostRoutes:
-#     base_url = 'http://127.0.0.1:8000/posts/'
+class TestPostRoutes:
+    base_url = 'http://127.0.0.1:8000/users/testcase/posts/'
 
-#     def test_get_all(self):
-#         response = requests.get(self.base_url)
+    def test_get_all(self):
+        response = requests.get(self.base_url)
 
-#         assert response.status_code == 200
-#         assert isinstance(response.json(), list)
+        assert response.status_code == 200
+        assert isinstance(response.json(), list)
