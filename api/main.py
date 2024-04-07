3617cbd89288324530b2e08f00a1af6e6f6bf0fa
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-04-07 20:58:13 -0300

    chore: routing routes

--- api/main.py
+++ api/main.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
 from api.routes import login, users
+from core.database import Base, engine
+from api.models.users import UserModel  # Não remova, dependencia de Base
+
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 api_router = APIRouter()
 api_router.include_router(login.router)
