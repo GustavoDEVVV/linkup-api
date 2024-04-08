3612323791bc0b63941fa2f4c1e37d5a531a86ca
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-04-07 21:01:29 -0300

    style: remove one line in header

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
 from api.main import api_router
-from core.database import Base, engine
-
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(api_router)
