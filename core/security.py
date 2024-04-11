a682a31e98156646e24253706df4f777959423d1
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-04-11 10:27:53 -0300

    feat: created access token and verify password

--- core/security.py
+++ core/security.py
@@ -1,9 +1,11 @@
 from passlib.context import CryptContext  # type: ignore
 from fastapi.security import OAuth2PasswordBearer
+from datetime import datetime, timedelta, timezone
+from jose import jwt, JWTError  # type: ignore
 
-SECRET_KEY = '62cb19571d5aba89bbcc372c56c336805870b1d649677f631fcb3b4ed89f5524'
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = '62cb19571d5aba89bbcc372c56c336805870b1d649677f631fcb3b4ed89f5524'
 
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
@@ -12,3 +14,18 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
