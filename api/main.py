c6d8f0be04f5e21c71a2cb249581238c7e3140a6
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-04-11 10:32:36 -0300

    feat: include posts routes/add event to startup

--- api/main.py
+++ api/main.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
-from api.routes import login, users
-from core.database import Base, engine
+
 from api.models.users import UserModel  # Não remova, dependencia de Base
+from api.models.posts import PostModel  # Não remova, dependencia de Base
+from api.routes import login, users, posts
+from core.database import Base, engine
+from core.init_data import init_db
 
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
@@ -9,3 +12,9 @@ Base.metadata.create_all(bind=engine)
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
+api_router.include_router(posts.router)
+
+
+@api_router.on_event('startup')
+async def startup():
+    await init_db()
