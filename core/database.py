5a4f98235074d0c132da05292dba4fbddc36b791
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-04-11 10:34:54 -0300

    refactor: moving get_db

--- core/database.py
+++ core/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine  # type: ignore
 from sqlalchemy.ext.declarative import declarative_base  # type: ignore
-from sqlalchemy.orm import sessionmaker  # type: ignore
+from sqlalchemy.orm import sessionmaker, Session  # type: ignore
+
 from core.config import settings  # type: ignore
 
 engine = create_engine(
@@ -15,3 +16,11 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+
+def get_db():
+    database = SessionLocal()
+    try:
+        yield database
+    finally:
+        database.close()
