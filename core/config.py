556095f78f012f564e44f941249eef7127834bc5
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-04-11 10:36:18 -0300

    feat: added the first superuser data

--- core/config.py
+++ core/config.py
@@ -2,5 +2,9 @@ class Settings():
     SQLALCHEMY_DATABASE_URI = 'sqlite:///./db.sqlite3'
     USERS_OPEN_REGISTRATION = True
 
+    FIRST_SUPERUSER_USERNAME: str = 'admin'
+    FIRST_SUPERUSER_PASSWORD: str = 'admin'
+    FIRST_SUPERUSER_EMAIL: str = 'admin@admin.com'
+
 
 settings = Settings()
