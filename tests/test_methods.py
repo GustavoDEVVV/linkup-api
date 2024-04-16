072db13c71794d1580e1862f8b56fe24bf80b0a4
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-04-16 19:29:24 -0300

    refactor: test structure refactor

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -23,87 +23,101 @@ from sqlalchemy.orm import sessionmaker
 import unittest
 
 
-class Utils:
-    def __init__(self) -> None:
-        engine = create_engine('sqlite:///:memory:')
+class TestUserCrud(unittest.TestCase):
+    def setUp(self):
+        engine = create_engine('sqlite:///./test.sqlite3')
         Session = sessionmaker(bind=engine)
         self.session = Session()
 
+        self.test_username = 'test'
+        self.test_password = '123@123'
+        self.test_email = 'test@email.com'
+
+        UserModel.metadata.drop_all(engine)
         UserModel.metadata.create_all(engine)
-        PostModel.metadata.create_all(engine)
+
+    def tearDown(self):
+        self.session.close()
+
+    def test_create_user(self):
+        user = UserCreate(
+            email=self.test_email,
+            username=self.test_username,
+            password=self.test_password,
+        )
+
+        created_user = create_user(session=self.session, user=user)
+        self.assertIsNotNone(created_user.id)
+
+
+    def test_create_superuser(self):
+        superuser = UserCreateSuperUser(
+            email=self.test_email,
+            username=self.test_username,
+            password=self.test_password,
+            is_superuser=True,
+        )
+        created_superuser = create_super_user(session=self.session, user=superuser)
+        self.assertIsNotNone(created_superuser.id)
+
+
+    def test_delete_user(self):
+        user = UserCreate(
+            email=self.test_email,
+            username=self.test_username,
+            password=self.test_password,
+        )
+
+        created_user = create_user(session=self.session, user=user)
+        delete_user(session=self.session, user=created_user)
+        deleted_user = get_user(session=self.session, user_id=created_user.id)
+
+        self.assertIsNone(deleted_user)
+
+
+class TestPostCrud(unittest.TestCase):
+    def setUp(self):
+        engine = create_engine('sqlite:///./test.sqlite3')
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
 
         self.test_username = 'test'
-        self.test_email = 'test@email.com'
         self.test_password = '123@123'
+        self.test_email = 'test@email.com'
 
         self.test_title = 'Testando titulo!'
         self.test_description = 'Testando descrição!'
 
-    def create_test_user(self):
+        UserModel.metadata.drop_all(engine)
+        UserModel.metadata.create_all(engine)
+
+
+    def tearDown(self):
+        self.session.close()
+
+
+    def test_create_post(self):
         user = UserCreate(
             email=self.test_email,
             username=self.test_username,
             password=self.test_password,
 
         )
-        return create_user(session=self.session, user=user) 
-
-    def create_test_super_user(self):
-        user = UserCreateSuperUser(
-            email=self.test_email,
-            username=self.test_username,
-            password=self.test_password,
-            is_superuser=True,
-        )
-        return create_super_user(session=self.session, user=user)
-
-    def create_test_post(self):
-        user_create = self.create_test_user()
 
         post = PostCreate(
             description=self.test_description,
             title=self.test_title,
         )
-        return insert_post(
-                        session=self.session,
-                        username=user_create.username,
-                        post=post, 
-                        )
 
+        created_user = create_user(session=self.session, 
+                                   user=user)
 
-# ==========================================================
+        created_post = insert_post(session=self.session, 
+                                   username=created_user.username, 
+                                   post=post)
 
-class TestUserCrud(unittest.TestCase):
-    def __init__(self, methodName: str = "runTest") -> None:
-        self.utils = Utils()
-        super().__init__(methodName)
+        self.assertIsNotNone(created_post.id)
 
-    def test_create_user(self):
-        user_create = self.utils.create_test_user()
-        self.assertIsNotNone(user_create.id)
-
-    def test_delete_user(self):
-        user_create = self.utils.create_test_user()
-        delete_user(session=self.utils.session, user=user_create)
-        user_deleted = get_user(session=self.utils.session, user_id=user_create.id)
-        self.assertIsNone(user_deleted)
-
-    def test_create_super_user(self):
-        superuser_create = self.utils.create_test_super_user()
-        self.assertIsNotNone(superuser_create.id)
-
-# ----------------------------------------------------------
-
-class TestPostCrud(unittest.TestCase):
-    def __init__(self, methodName: str = "runTest") -> None:
-        self.utils = Utils()
-        super().__init__(methodName)
-
-    def test_create_post(self):
-        post_create = self.utils.create_test_post()
-        self.assertIsNotNone(post_create.id)
-
-# ==========================================================
 
 if __name__ == '__main__':
     unittest.main()
