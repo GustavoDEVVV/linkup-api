3682e7c223b3ed4c8ff6a00a4b65932004257846
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-04-20 19:32:21 -0300

    refactor: rename login > auth/moving signup to auth routes

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -1,21 +1,25 @@
 from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session 
+from sqlalchemy.orm import Session
 from datetime import timedelta
 from fastapi import APIRouter
 
 from core.security import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
 from api.schemas.token import Token
-from api.crud.login import authenticate_user
+from api.crud.login import authenticate_user, get_user_by_username
+from api.crud.users import create_user
 from core.database import get_db
+from api.schemas.users import UserCreate, UserOutPut
+from core.config import settings
+
 
 router = APIRouter(
-    tags=['token']
+    tags=['auth']
 )
 
 
-@router.post('/token')
+@router.post('/login')
 async def login_for_access_token(
         form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: Session = Depends(get_db)) -> Token:
     user = authenticate_user(session, form_data.username, form_data.password)
@@ -30,3 +34,31 @@ async def login_for_access_token(
         data={'sub': user.username},  expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type='bearer')
+
+
+@router.post('/signup', response_model=UserOutPut)
+async def register_user(user_in: UserCreate, session: Session = Depends(get_db)):
+
+    if not settings.USERS_OPEN_REGISTRATION:
+        raise HTTPException(
+            status_code=403,
+            detail="Open user registration is forbidden on this server",
+        )
+
+    user = get_user_by_username(session=session, username=user_in.username)
+
+    if user:
+        raise HTTPException(
+            status_code=400,
+            detail="The user with this email already exists in the system",
+        )
+
+    user = create_user(session, user_in)
+
+    user_response = UserOutPut(
+        id=user.id,
+        username=user.username,
+        email=user.email,
+    )
+
+    return user_response
