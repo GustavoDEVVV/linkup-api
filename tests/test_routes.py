25c6ee2b3aec3378c97b305f842b8e7cefe274a9
Gustavo Domingues <142946339+GustavoDEVVV@users.noreply.github.com>
2024-05-23 17:08:09 -0300

    TESTE, CLEANP Concertando os erros no testes de sistemas Post

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -42,44 +42,44 @@ class TestPostRoutes:
     url_one = 'http://127.0.0.1:8000/users/username'
     url_signup = 'http://127.0.0.1:8000/users/signup'
 
-    username_test = 'testUsername'
+    titulo_teste: 'Testando_Post'
     password_test = 'testUsername'
     email_test = 'test@username.com'
+    conteudo_teste: 'testando_os_posts'
 
     def get_auth_token(self):
-        response = self.client.post('/login', data=json.dumps ({ #envia uma solicitação post para o login enviando email e senha do usuario
-            'email': 'testuser@exemple.com',
-            'password': 'password'
+        response = self.client.post('/login', data=json.dumps ({
+            "email": self.email_test,
+            "password": self.password_test
         }), content_type='application/json')
 
-        data = json.loads(response.data) #converte os dados em json
-        return data['access_token'] #retorna o token de accesso
+        data = json.loads(response.data)
+        return data['access_token']
     
     def test_creation_post(self):
-        token = self.get_auth_token() #chama o token valido criado acima para solicitar a criação
+        token = self.get_auth_token()
         response = self.client.post('/post', data=json.dumps({
-            'titulo': 'Testando Post',          #enviando uma solicitação para rota post e enviando o titulo e conteudo da mensagem em JSON, passa também o token  
-            'conteudo': 'testando os posts'
+            'titulo': self.titulo_teste,
+            'conteudo': self.conteudo_teste
         }), headers={'Autorização': f'Bearer {token}'}, content_type='application/json')
-        self.assertEqual(response.status_code, 201) #verifica se foi criado
-        self.assertIn('id', json.loads(response.data)) #verifica se tem o campo id pra saber se foi criado o post
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('id', json.loads(response.data))
 
     def test_delet_post(self):
 
-        token = self.get_auth_token() #pega o token de autenticação
+        token = self.get_auth_token()
 
-        post_response = self.client.post('/post', data=json.dumps({ #faz uma solicitação a rota post enviando os dados e a autorização nas cabeças da aplicação
-            'titulo': 'Testando Post',
-            'conteudo': 'testando os posts'
+        post_response = self.client.post('/post', data=json.dumps({
+            'titulo': self.titulo_teste,
+            'conteudo': self.conteudo_teste
         }), headers= {'Autorização': f'bearer {token}'}, content_type='application/json')
-        post_id = json.loads(post_response.data)['id'] #extraindo o id post a partir da requisição
+        post_id = json.loads(post_response.data)['id']
 
         delete_response = self.client.delete(f'/posts/{post_id}', headers={'Authorization': f'Bearer {token}'})
-        self.assertEqual(delete_response.status_code, 204) #end point expecifico do post criado
+        self.assertEqual(delete_response.status_code, 204)
 
-        # Verify the post is deleted
         get_response = self.client.get(f'/posts/{post_id}', headers={'Authorization': f'Bearer {token}'})
-        self.assertEqual(get_response.status_code, 404) #requisição get para o mesmo endpoint anterior
+        self.assertEqual(get_response.status_code, 404) 
 
 
 if __name__ == '__main__':
